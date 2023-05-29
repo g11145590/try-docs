da796ee29017c9f21687364b54648461a51ced74
MooGoo <moogoo78@gmail.com>
2023-05-29 14:40:32 +0800

    add assets folder

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build
-docker build -t my-try-docs . --no-cache
+#docker build -t my-try-docs . --no-cache
+docker run --rm -it -v ${PWD}:/docs my-try-docs build
